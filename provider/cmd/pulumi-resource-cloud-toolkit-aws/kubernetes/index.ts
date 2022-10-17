21d757bae1de224385cee008a09959486b8f1fbb
Pasquale Toscano <pasquale.toscano@astrokube.com>
2022-10-17 15:28:35 +0200

    Register IngressNginx in provider

--- provider/cmd/pulumi-resource-cloud-toolkit-aws/kubernetes/index.ts
+++ provider/cmd/pulumi-resource-cloud-toolkit-aws/kubernetes/index.ts
@@ -4,5 +4,21 @@ import { ClusterAddons, ClusterAddonsArgs } from "./clusterAddons";
 import { ArgoCD, ArgoCDArgs } from "./argocd";
 import { CertManager, CertManagerArgs } from "./certManager";
 import { ExternalDns, ExternalDnsArgs } from "./externalDns";
+import { IngressNginx, IngressNginxArgs } from "./ingressNginx";
 
-export { Cluster, ClusterArgs, ClusterAddons, ClusterAddonsArgs, ArgoCD, ArgoCDArgs, ExternalDns, ExternalDnsArgs, CertManager, CertManagerArgs, NodeGroup, NodeGroupArgs };
+export {
+  ArgoCD,
+  ArgoCDArgs,
+  CertManager,
+  CertManagerArgs,
+  Cluster,
+  ClusterAddons,
+  ClusterAddonsArgs,
+  ClusterArgs,
+  ExternalDns,
+  ExternalDnsArgs,
+  IngressNginx,
+  IngressNginxArgs,
+  NodeGroup,
+  NodeGroupArgs
+};
