d0699ae1f37b07b864e4f239ee672d423eb45580
Pasquale Toscano <pasquale.toscano@astrokube.com>
2022-10-25 00:40:51 +0200

    Deploy Calico Installation as CustomResource
    Fix Calico outputs

--- provider/cmd/pulumi-resource-cloud-toolkit-aws/kubernetes/calico.ts
+++ provider/cmd/pulumi-resource-cloud-toolkit-aws/kubernetes/calico.ts
@@ -10,117 +10,124 @@ export { CalicoArgs };
 
 export class Calico extends ApplicationAddon<CalicoArgs> {
 
-    public readonly namespace?: kubernetes.core.v1.Namespace;
-    public readonly application: kubernetes.apiextensions.CustomResource;
+  public readonly namespace?: kubernetes.core.v1.Namespace;
+  public readonly application: kubernetes.apiextensions.CustomResource;
+  public readonly installation: kubernetes.apiextensions.CustomResource;
 
-    constructor(name: string, args: CalicoArgs, opts?: pulumi.ResourceOptions) {
-        super("cloud-toolkit-aws:kubernetes:Calico", name, args, opts);
+  constructor(name: string, args: CalicoArgs, opts?: pulumi.ResourceOptions) {
+    super("cloud-toolkit-aws:kubernetes:Calico", name, args, opts);
 
-        const resourceOpts = pulumi.mergeOptions(opts, {
-            parent: this,
-        });
+    const resourceOpts = pulumi.mergeOptions(opts, {
+      parent: this,
+    });
 
-        this.namespace = this.setupNamespace(resourceOpts);
-        this.application = this.setupApplication(resourceOpts);
+    this.namespace = this.setupNamespace(resourceOpts);
+    this.application = this.setupApplication(resourceOpts);
 
-        this.registerOutputs({
-            chart: this.application,
-            namespace: this.namespace,
-        })
-    }
+    const installationOpts = pulumi.mergeOptions(resourceOpts, {
+      dependsOn: [
+        this.application,
+      ]
+    });
+    this.installation = this.setupInstallation(resourceOpts);
 
-    protected validateArgs(a: CalicoArgs): CalicoArgs {
-        const args = defaultsDeep({ ...a }, defaultArgs);
+    this.registerOutputs({
+      namespace: this.namespace,
+      application: this.application,
+      installation: this.installation,
+    })
+  }
 
-        return args;
-    }
+  protected validateArgs(a: CalicoArgs): CalicoArgs {
+    const args = defaultsDeep({ ...a }, defaultArgs);
 
-    protected getApplicationSpec(): any {
-        return {
-            project: "default",
-            source: {
-                repoURL: "https://docs.projectcalico.org/charts",
-                chart: "tigera-operator",
-                targetRevision: "v3.24.0",
-                helm: {
-                    releaseName: this.args.name,
-                    parameters: [
-                        {
-                            name: "apiServer.enabled",
-                            value: "false",
-                        },
-                        {
-                            name: "installation.enabled",
-                            value: "true",
-                        },
-                        {
-                            name: "installation.calicoNetwork.bgp",
-                            value: "Disabled",
-                        },
-                        {
-                            name: "installation.calicoNetwork.linuxDataplane",
-                            value: "Iptables",
-                        },
-                        {
-                            name: "installation.cni.type",
-                            value: "AmazonVPC",
-                        },
-                        {
-                            name: "installation.cni.ipam.type",
-                            value: "AmazonVPC",
-                        },
-                        {
-                            name: "installation.controlPlaneReplicas",
-                            value: "2",
-                        },
-                        {
-                            name: "installation.kubernetesProvider",
-                            value: "EKS",
-                        },
-                        {
-                            name: "installation.nodeUpdateStrategy.rollingUpdate.maxUnavailable",
-                            value: "1",
-                        },
-                        {
-                            name: "installation.nodeUpdateStrategy.type",
-                            value: "RollingUpdate",
-                        },
-                        {
-                            name: "installation.nonPrivileged",
-                            value: "Disabled",
-                        },
-                        {
-                            name: "installation.variant",
-                            value: "Calico",
-                        },
-                    ],
-                },
+    return args;
+  }
+
+  protected getApplicationSpec(): any {
+    return {
+      project: "default",
+      source: {
+        repoURL: "https://docs.projectcalico.org/charts",
+        chart: "tigera-operator",
+        targetRevision: "v3.24.0",
+        helm: {
+          releaseName: this.args.name,
+          parameters: [
+            {
+              name: "apiServer.enabled",
+              value: "false",
             },
-            destination: {
-                server: "https://kubernetes.default.svc",
-                namespace: "system-calico",
+            {
+              name: "installation.enabled",
+              value: "false",
             },
-            syncPolicy: {
-                automated: {
-                    prune: true,
-                    selfHeal: true,
-                    allowEmpty: false,
-                },
-                retry: {
-                    limit: 10,
-                    backoff: {
-                        duration: "5s",
-                        factor: 2,
-                        maxDuration: "1m",
-                    },
-                },
-                syncOptions: [
-                    "Validate=false",
-                    "PrunePropagationPolicy=foreground",
-                    "PruneLast=true",
-                    "Replace=true",
-                ],
+          ],
+        },
+      },
+      destination: {
+        server: "https://kubernetes.default.svc",
+        namespace: "system-calico",
+      },
+      syncPolicy: {
+        automated: {
+          prune: true,
+          selfHeal: true,
+          allowEmpty: false,
+        },
+        retry: {
+          limit: 10,
+          backoff: {
+            duration: "5s",
+            factor: 2,
+            maxDuration: "1m",
+          },
+        },
+        syncOptions: [
+          "Validate=false",
+          "PrunePropagationPolicy=foreground",
+          "PruneLast=true",
+          "Replace=true",
+        ],
+      },
+    };
+  }
+
+  private setupInstallation(opts: pulumi.CustomResourceOptions): kubernetes.apiextensions.CustomResource {
+    return new kubernetes.apiextensions.CustomResource(
+      this.name,
+      {
+        apiVersion: "operator.tigera.io/v1",
+        kind: "Installation",
+        metadata: {
+          name: "default",
+        },
+        spec: {
+          calicoNetwork: {
+            bgp: "Disabled",
+            linuxDataplane: "Iptables",
+          },
+          cni: {
+            type: "AmazonVPC",
+            ipam: {
+              type: "AmazonVPC",
             },
-        };
-    }
+          },
+          controlPlaneReplicas: 2,
+          kubernetesProvider: "EKS",
+          nodeUpdateStrategy: {
+            rollingUpdate: {
+              maxUnavailable: 1
+            },
+            type: "RollingUpdate",
+          },
+          nonProvileged: "Disabled",
+          variant: "Calico",
+        },
+      },
+      pulumi.mergeOptions(opts, {
+        provider: this.args.k8sProvider,
+      })
+    );
+  }
 }
