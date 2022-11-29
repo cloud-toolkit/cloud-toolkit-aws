8905a712744580c44d32cabe4ad57df555e64654
Eduardo Sánchez López <eduardo.sanchez.eslp@gmail.com>
2022-11-29 11:29:35 +0100

    Removed old tests based in go and added missing whitespaces at the end of a file

--- acceptance_tests/test_suites/kubernetes/__init__.robot
+++ acceptance_tests/test_suites/kubernetes/__init__.robot
@@ -4,4 +4,3 @@ Suite Setup      I deploy the Kubernetes Cluster with Pulumi
 Suite Teardown   I destroy the Kubernetes Cluster with Pulumi
 
 Resource         ../../resources/keywords_library/kubernetes.robot
- 
--- tests/kubernetes/resources/index.ts
+++
@@ -1,8 +0,0 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as ct from "@cloud-toolkit/cloud-toolkit-aws";
-
-const config = new pulumi.Config();
-const name = pulumi.getStack();
-const retention = config.getNumber("retention");
-const queue = new ct.serverless.Queue(name, {messageRetentionSeconds: retention});
-
